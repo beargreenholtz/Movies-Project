89bd62af48fb117aa4a7228844431d00af8f80bd
beargreenholtz <beargreenholtz@gmail.com>
2022-12-03 19:57:50 +0200

    fixed axios error on login

--- frontend/src/components/containers/Login/SignInForm/SignInForm.tsx
+++ frontend/src/components/containers/Login/SignInForm/SignInForm.tsx
@@ -10,30 +10,31 @@ import { AuthContext } from '../../../../context/authContext';
 interface IProps {}
 
 const SignInForm: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
-	const [isErrorLogin, setIsErrorLogin] = useState<boolean>(false);
+	// const [isErrorLogin, setIsErrorLogin] = useState<boolean>(false);
+	const [error, setError] = useState<string>('');
 	const auth = useContext(AuthContext);
 	const navigate = useNavigate();
 
 	const onLogin = async (values: FormikValues) => {
 		try {
 			const responseData = await axios.post(`http://localhost:5000/login/login`, values).then((res) => {
-				console.log(res);
+				// console.log(res);
 				auth.login(res.data.userId, res.data.token);
 				navigate('/');
 			});
 		} catch (error) {
-			setIsErrorLogin(true);
 			if (axios.isAxiosError(error)) {
-				let serverError = error as AxiosError;
+				let serverError = error as AxiosError<{ message: string }>;
 				if (serverError && serverError.response) {
-					let err = serverError.response.data;
+					let err = serverError.response.data.message;
+					setError(err);
 					console.log(err);
 				}
 			}
 		}
 	};
 
-	return <SignInFormView isError={isErrorLogin} onSubmit={onLogin} />;
+	return <SignInFormView error={error} onSubmit={onLogin} />;
 };
 
 SignInForm.displayName = 'SignInForm';
